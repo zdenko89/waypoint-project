2cc3e81fafac899855d6873ff4e3c48af650e912
zdenko89 <zbraz001@gold.ac.uk>
2017-12-27 20:13:59 +0000

    added second character

--- Assets/Scripts/Waypoint.cs
+++ Assets/Scripts/Waypoint.cs
@@ -1,14 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 using UnityEngine.AI; // Now able to use fucntions within unity for AI
 
 public class Waypoint : MonoBehaviour {
 
     NavMeshAgent NavPoint; // Declared the navMeshAgent to be called NavPoint
-    public Transform[] Waypoints;
-    public int curr_loc; // Current location
-    public float stop_distance;
+    public Transform[] WayPoints;
+
+
+    public int current_loc; // Current location
+    public float stop_distance; // max distance from waypoint
 
 	
 	void Start () {
@@ -21,33 +24,33 @@ public class Waypoint : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 
-        float distance = Vector3.Distance(transform.position, Waypoints[curr_loc].position); // Finds the distance between the player location and waypoint
+        float distance_between = Vector3.Distance(transform.position, WayPoints[current_loc].position); // Finds the distance between the player location and waypoint
 
-        transform.LookAt(Waypoints[curr_loc]); // Face the waypoint selected
+        transform.LookAt(WayPoints[current_loc]); // Face the waypoint selected
 
-        if (distance <= stop_distance) // once the player has arrived at the waypoint and the distance is less or equal to stop_distance then...
+        if (distance_between <= stop_distance) // once the player has arrived at the waypoint and the distance is less or equal to stop_distance then...
         {
-            curr_loc += 1; // add 1
+            current_loc += 1; // add 1
         }
 
-        if (curr_loc >= Waypoints.Length) // once the player has arrived at the waypoint but the distance is more than stop_distance then...
+        if (current_loc >= WayPoints.Length) // once the player has arrived at the waypoint but the distance is more than stop_distance then...
         {
-            curr_loc = 0; // reset it and make it equal to 0
+            current_loc = 0; // reset it and make it equal to 0
         }
 
-        if (curr_loc == 0) // if the current location of the waypoint is 0 then...
+        if (current_loc == 0) // if the current location of the waypoint is 0 then...
         {
-            NavPoint.SetDestination(Waypoints[0].position); // use NavmeshAgent to set pathway 0 in the Waypoints array
+            NavPoint.SetDestination(WayPoints[0].position); // use NavmeshAgent to set pathway 0 in the Waypoints array
         }
 
-        if (curr_loc == 1) // // if the current location of the waypoint is 1 then...
+        if (current_loc == 1) // // if the current location of the waypoint is 1 then...
         {
-            NavPoint.SetDestination(Waypoints[1].position); // use NavmeshAgent to set pathway 1 in the Waypoints array 
+            NavPoint.SetDestination(WayPoints[1].position); // use NavmeshAgent to set pathway 1 in the Waypoints array 
         }
 
-        if (curr_loc == 2) // // if the current location of the waypoint is 2 then...
+        if (current_loc == 2) // // if the current location of the waypoint is 2 then...
         {
-            NavPoint.SetDestination(Waypoints[2].position); // use NavmeshAgent to set pathway 2 in the Waypoints array
+            NavPoint.SetDestination(WayPoints[2].position); // use NavmeshAgent to set pathway 2 in the Waypoints array
         }
 
     }
